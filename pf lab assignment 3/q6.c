8520232de9e32d4fd857ea771f00605bd2fe5e1d
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2024-09-05 08:56:40 -0700

    Update q6.c

--- pf lab assignment 3/q6.c
+++ pf lab assignment 3/q6.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main()
 {
+	//declaring variables
 	float X1;
 	float Y1;
 	float X2;
 	float Y2;
 	float Slope; 
 	
+	//taking input from the user
 	printf(" enter the first x cordinate ");
 	scanf("%f",&X1);
 	printf(" enter the first y cordinate ");
@@ -15,6 +17,8 @@ int main()
 	scanf("%f",&X2);
 	printf(" enter the second y cordinate ");
 	scanf("%f",&Y2);
+
+	//calculating and printing the slope
 	Slope=((Y2-Y1)/(X2-X1));
 	printf(" slope is %.3f ",Slope);
 	return 0;
